f89e3e139c3d538858d980aa055581c20f4fb4c3
dubipy <qnfrha999@naver.com>
2022-07-29 00:03:54 +0900

    .

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
-window.addEventListener("load",() => {
-    const grid = new Isotope("section",{ //부모 요소 지정
+window.addEventListener("load",()=>{
+    const grid = new Isotope("section",{
         itemSelector: "article",
-        columWidth: "article",
-        transitionDuration: "0.5s"// 호출구문
+        columnWidth: "article",
+        transitionDuration: "0.5s"
     })
 
     const btns = document.querySelectorAll("main ul li");
 
-    btns.forEach((btn, index) => {
-        btn.addEventListener("click", e => {
+    btns.forEach(btn=>{
+        btn.addEventListener("click",e=>{
             e.preventDefault();
 
-            const btn_a = e.currentTarget.querySelector("a");
-            const a_href = btn_a.getAttribute("href") //속성값, 속성명 가져오기
-            console.log(a_href)
-
-            grid.arrange({filter: a_href});
+            const isOn = e.currentTarget.classList.contains("on");
+            if(isOn) return;
+            activation(e);            
         })
     })
-    })
 
-//필터링 기능 적용
+    function activation(event){
+        for(let btn of btns) btn.classList.remove("on");
+        event.currentTarget.classList.add("on");
+
+        const btn_a = event.currentTarget.querySelector("a");
+        const a_href = btn_a.getAttribute("href");     
+
+        grid.arrange({filter: a_href});
+    }
+})
